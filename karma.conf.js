c250c4006b74174d7989d6af2f153d8459503bd0
Nicola Zanon <nicola.zanon86@gmail.com>
2017-01-02 18:15:09 +0000

    Set up Karma for testing

---
+++ karma.conf.js
@@ -0,0 +1,43 @@
+// const webpack = require('webpack');
+
+module.exports = (config) => {
+  config.set({
+    browsers: ['Chrome'], // run in Chrome
+    singleRun: true, // just run once by default
+    frameworks: ['mocha', 'chai'], // use the mocha test framework
+    files: [
+      'tests.webpack.js', // just load this file
+    ],
+    preprocessors: {
+      'tests.webpack.js': ['webpack', 'sourcemap'], // preprocess with webpack and our sourcemap loader
+    },
+    reporters: ['progress', 'coverage'],
+    webpack: { // kind of a copy of your webpack config
+      devtool: 'inline-source-map', // just do inline source maps instead of the default
+      module: {
+        loaders: [
+          { test: /\.js$/, loader: 'babel-loader' },
+        ],
+      },
+    },
+    coverageReporter: {
+      check: {
+        global: {
+          statements: 11,
+          branches: 0,
+          functions: 0,
+          lines: 11,
+        },
+      },
+      reporters: [
+        { type: 'lcov', dir: 'coverage/', subdir: '.' },
+        { type: 'json', dir: 'coverage/', subdir: '.' },
+        { type: 'text-summary' },
+      ],
+    },
+    webpackMiddleware: { noInfo: true },
+    colors: true,
+    concurrency: Infinity,
+    logLevel: config.LOG_DEBUG,
+  });
+};
